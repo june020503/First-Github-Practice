2aa4ab2493bb60cfa43482e0263c6000e6cfe71a
june020503 <june020503@gmail.com>
2021-09-23 12:03:07 +0900

    Cprogramming_homework#1

---
+++ date.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("2021/09/07\n");
+
+	return 0;
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int studentNum=20211516;
+
+	printf("%d Kim youngjune\n",studentNum);
+	
+	return 0; 
+}
